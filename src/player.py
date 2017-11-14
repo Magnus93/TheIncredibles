d97ec2cbedc8388c34f000faf3b63c9dc0b343e9
LinneaGunnarsson <linnea.gunnarsson@live.com>
2017-11-14 21:13:48 +0100

    Add files via upload

--- src/player.py
+++ src/player.py
@@ -1,26 +1,45 @@
-class Player: 
-    def __init__player(self, player_id, position, color, name, lives, shot_freq):
+import pygame
+from math import *
+
+class player: 
+    def __init__(self, player_id, position, color, name):
         self.player_id = player_id
         self.position = position
         self.speed = (0,0)
         self.angle = 0
         self.angle_speed = 0
-        
         self.color = color
         self.lives = 3
-        
-        self.shot_freq = shot_freq 
+        self.shot_freq = 2.0
         self.name = name
 
     def update_position(self):
-        pass
+        # Flytta ut icke position events
+        for event in  pygame.event.get():
+        #    if event.type == pygame.QUIT:
+         #       print("quit")
+            if event.type == pygame.KEYDOWN:
+                pressed_key = pygame.key.get_pressed()
+                if pressed_key[pygame.K_UP]:
+                    print("UP")
+                    self.position = (self.position[0], self.position[1]-1)
+                if pressed_key[pygame.K_DOWN]:
+                    print("down")
+                    self.position = (self.position[0], self.position[1]+1)
+                if pressed_key[pygame.K_LEFT]:
+                    print("LEFT")
+                    self.position = (self.position[0]-1, self.position[1])
+                if pressed_key[pygame.K_RIGHT]:
+                    print("RIGHT")
+                    self.position = (self.position[0]+1, self.position[1])
+        
 
     def check_collision(self, other_player):
         pass
 
-    def draw_player(self):
-        pass
-    
+    def draw(self):
+        pygame.draw.circle(screen,self.color, self.position, 10) 
+        
     def shoot(self):
         pass
 
@@ -33,3 +52,12 @@ class Player:
             self.die()
         
 
+if __name__ == '__main__':
+    screen = pygame.display.set_mode((800,600))
+    p1 = player(1, (20,30), (255,0,255), "Nea")
+   
+    while True:
+        screen.fill((0,0,0))
+        p1.draw()
+        p1.update_position()
+        pygame.display.flip()
