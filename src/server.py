2bb8d80493fbcf9d820ddab67bd747228bc2e4d2
andraste <Andra-Stefania.Negus.0972@student.uu.se>
2017-11-21 12:31:01 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/server.py
+++ src/server.py
@@ -4,7 +4,7 @@
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 8089))
+serversocket.bind((socket.gethostname(), 8089))
 serversocket.listen(5) # become a server socket, maximum 5 connections
 
 while True:
