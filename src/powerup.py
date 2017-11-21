4e3b292bf3a21891f7db6db97d93476376b44c3c
Magnus <magnus.ja.gustafsson@gmail.com>
2017-11-21 13:06:44 +0100

    draw f

--- src/powerup.py
+++ src/powerup.py
@@ -1,41 +1,51 @@
 import pygame
 
 class powerup:
-	
+
 	def __init__(self, position, effect):
-	        self.position = position
-	        self.effect = effect
+		self.position = position
+		self.effect = effect
+		self.size = 24
 
-        def draw_life(self,pos, color):
-                pygame.draw.line(screen, color, pos, (pos[0],pos[1]+8),1)
-                pygame.draw.line(screen, color, pos, (pos[0]+8,pos[1]),1)
-                pygame.draw.line(screen, color, pos, (pos[0]-8,pos[1]),1)
-                pygame.draw.line(screen, color, pos, (pos[0],pos[1]-8),1)
-                pygame.draw.rect(screen, color, (pos[0]-12, pos[1]-12,24,24),2)
+	def draw(self):
+		if self.effect == "life":
+			self.draw_life()
+		elif self.effect == "blaster":
+			self.draw_blaster()
 
-        def draw_blaster(self):
-                pos = self.position
-                self.draw_life(pos, (255,0,255))
-                pygame.draw.circle(screen, (255,0,255), (pos[0]+1, pos[1]+1), 8, 1)
+	def draw_life(self):
+		pos = self.position
+		pygame.draw.rect(screen, (180,180,180), (pos[0]-11, pos[1]-11, 22, 22), 0)
+		pygame.draw.rect(screen, (102,102,102), (pos[0]-12, pos[1]-12, 24, 24), 4)
+		pygame.draw.line(screen, (255,42,42), (pos[0], pos[1]-8), (pos[0], pos[1]+8), 5)
+		pygame.draw.line(screen, (255,42,42), (pos[0]-8, pos[1]), (pos[0]+8 ,pos[1]), 5)
 
-        def pickup(self): 
-                #get_effect()
-                # Go away when player is in same position
-                pass
 
-        def get_effect(self):
-                if self.effect =="life":
-                        player.lives +=1
-                if self.effect == "blaster":
-                        player.shot_freq *= 2
+	def draw_blaster(self):
+		pos = self.position
+		pygame.draw.rect(screen, (180,180,180), (pos[0]-11, pos[1]-11, 22, 22), 0)
+		pygame.draw.rect(screen, (102,102,102), (pos[0]-12, pos[1]-12, 24, 24), 4)
+		pygame.draw.line(screen, (255,42,42), (pos[0], pos[1]-9), (pos[0], pos[1]+9), 1)
+		pygame.draw.line(screen, (255,42,42), (pos[0]-9, pos[1]), (pos[0]+9 ,pos[1]), 1)
+		pygame.draw.circle(screen, (255,0,255), (pos[0]+1, pos[1]+1), 7, 1)
+
+	def pickup(self):
+		#get_effect()
+		# Go away when player is in same position
+		pass
+
+	def get_effect(self):
+		if self.effect =="life":
+			player.lives +=1
+		elif self.effect == "blaster":
+			player.shot_freq *= 2
 
 
 if __name__ == '__main__':
-       screen = pygame.display.set_mode((800,600))
-       p = powerup((300,300), "life")
-       while(True):
-        p.draw_life((400,400), (255, 0, 0))
-        p.draw_blaster()
-        pygame.display.flip()
-
-
+	screen = pygame.display.set_mode((800,600))
+	pl = powerup((300,300), "life")
+	pb = powerup((340,300), "blaster")
+	while(True):
+		pl.draw()
+		pb.draw()
+		pygame.display.flip()
