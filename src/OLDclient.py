ef207d98ed024211f585dcd05a966c55638b47cb
andraste <andra_stefania@yahoo.com>
2017-12-08 12:03:10 +0100

    Delete OLDclient.py

--- src/OLDclient.py
+++
@@ -1,8 +0,0 @@
-#BASIC client
-#run this one after you've ran server.py
-import socket
-
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect((socket.gethostbyname('localhost'), 8089))
-msg = raw_input('type anything and click enter... ')
-clientsocket.send(msg)
