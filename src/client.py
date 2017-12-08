7c15a3a628e28c1f3c794417708aa92a470e82d5
vahe2002 <florencia73000@gmail.com>
2017-12-08 12:21:21 +0300

    New client

--- src/client.py
+++ src/client.py
@@ -6,28 +6,35 @@ from player import *
 
 #source: https://pymotw.com/2/socket/udp.html
 # Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+class client:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_address = ('localhost', 10000)
+    #change when logic for creating a player from user input is ready
+        self.player = player(1, (300,300), (255,0,255), "Valeria")
+        self.list_of_players = {}
 
-server_address = ('localhost', 10000)
-message = 'This is the message.  It will be repeated.'
+    def start_client(self):
+        #client_for_player = client();
+        while (self.player.lives > 0):
+            try:
+                self.send_player()
+                self.recieve_player()
+            except KeyboardInterrupt:
+                raise
+    def send_player(self):
+        data = pickle.dumps(self.player)
+        self.sock.sendto(data, self.server_address)
+        print >>sys.stderr, 'sent player to server'
+    def recieve_player(self):
+        print >>sys.stderr, 'waiting to receive'
+        data, server = self.sock.recvfrom(4096)
+        unpickled_playr=pickle.loads(data)
+        print >>sys.stderr, 'received "%s"' % unpickled_playr.name
 
-try:
 
-    # Send data
-    print >>sys.stderr, 'sending "%s"' % message
-    #sent = sock.sendto(message, server_address)
 
-    #p = player(player_id=1, position=(300,300), color=(255,0,255), name='Nea') 
-    p = player(1, (300,300), (255,0,255), "Nea")  
-    pl=pickle.dumps(p)
-    sock.sendto(pl,server_address)
-    
-    # Receive response
-    print >>sys.stderr, 'waiting to receive'
-    data, server = sock.recvfrom(4096)
-    unpickled_playr=pickle.loads(data)
-    print >>sys.stderr, 'received "%s"' % unpickled_playr.name
+#message = 'This is the message.  It will be repeated.'
 
-finally:
-    print >>sys.stderr, 'closing socket'
-    sock.close()
+client_for_player = client()
+client_for_player.start_client()
